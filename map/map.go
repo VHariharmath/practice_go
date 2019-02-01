5ff3b1afb0e61b54e7ddf1e95e3bbe9bf3d4e303
vinayak hariharmath <vinayak.hariharmath@gmail.com>
2019-02-01 10:06:07 +0530

    example to use map premitive

--- map/map.go
+++ map/map.go
@@ -14,9 +14,18 @@ func main() {
 	fmt.Println("Availability for the match")
 	for _, item := range s {
 		if _, ok := m[item]; ok {
-			fmt.Printf("%v\t : Available \n", item)
+			fmt.Printf("%v\t : Present in the squad and age = %v \n", item, m[item])
 		} else {
-			fmt.Printf("%v\t : Not available \n", item)
+			fmt.Printf("%v\t : Not Present in the squad\n", item)
 		}
+	}
+
+	fmt.Println("Adding Manish and Karun to the team")
+	m["Manish"] = 24
+	m["Karun"] = 22
+
+	fmt.Println("Squad now: ")
+	for k, v := range m {
+		fmt.Println(k, v)
 	}
 }
