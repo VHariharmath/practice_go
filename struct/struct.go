3adfa3574fdddc4ccb907c1674a91745c397b704
vinayak hariharmath <vinayak.hariharmath@gmail.com>
2019-02-04 20:13:04 +0530

    declaring a struct, emedding structs (inheritance), promotion and anonymous struct

--- struct/struct.go
+++ struct/struct.go
@@ -8,9 +8,25 @@ type Career struct {
 	cents int
 }
 type person struct {
+	// Anonymous field, no field name
+	Father
 	first  string
 	second string
 	career Career
+}
+
+type Father struct {
+	first  string
+	second string
+}
+
+type playerName struct {
+	first  string
+	second string
+}
+type player struct {
+	playerName
+	age int
 }
 
 type emptyStruct struct {
@@ -18,6 +34,10 @@ type emptyStruct struct {
 
 func main() {
 	p1 := person{
+		Father: Father{
+			first:  "xyz",
+			second: "abc",
+		},
 		first:  "Virat",
 		second: "Kohli",
 		career: Career{
@@ -28,8 +48,26 @@ func main() {
 	}
 	fmt.Println(p1)
 
-	// Anonymous struct
-	p2 := struct {
+	fmt.Println(p1.first, p1.Father.first)
+
+	p2 := player{
+		playerName: playerName{
+			first:  "Rohit",
+			second: "Sharma",
+		},
+		age: 30,
+	}
+
+	// If "first" field name is not present in parent struct
+	// then "first" named field from embedded struct can be used
+	// directly like p2.first rather than p2.playerName.first
+	// Below both prints same info
+	fmt.Println(p2.first, p2.second, p2.age)
+	fmt.Println(p2.playerName.first, p2.playerName.second, p2.age)
+
+	// Anonymous struct, if struct is only used in only one place and not
+	// require to declare will have this type of declaration and defination
+	p3 := struct {
 		first  string
 		second string
 		career Career
@@ -47,5 +85,5 @@ func main() {
 		},
 	}
 
-	fmt.Println(p2)
+	fmt.Println(p3)
 }
